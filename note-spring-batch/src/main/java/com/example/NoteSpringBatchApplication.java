c9fbd79b4b87bb6f995099f765c4d64277ec1a6e
sunxiaochun-code <13050571762@163.com>
2021-05-08 14:41:02 +0800

    spring batch

---
+++ note-spring-batch/src/main/java/com/example/NoteSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteSpringBatchApplication.class, args);
+    }
+
+}
