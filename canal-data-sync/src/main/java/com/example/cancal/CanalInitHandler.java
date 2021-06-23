01f626cabe9860dbbaa292439670fdbf02d27bc9
sunxiaochun-code <13050571762@163.com>
2021-06-23 20:35:21 +0800

    note-example 数据同步

--- canal-data-sync/src/main/java/com/example/cancal/CanalInitHandler.java
+++ canal-data-sync/src/main/java/com/example/cancal/CanalInitHandler.java
@@ -3,6 +3,7 @@ package com.example.cancal;
 import com.example.handler.MessageHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
@@ -16,10 +17,11 @@ public class CanalInitHandler {
     private final static List<CanalClient> canalClientList = new ArrayList<>();
 
     @Autowired
-    private MessageHandler messageHandler;
+    @Qualifier("messageHandlerToMongo")
+    private MessageHandler messageHandlerToMongo;
 
     public void initCanalStart() {
-        CanalClient client = new CanalClient(messageHandler);
+        CanalClient client = new CanalClient(messageHandlerToMongo);
         client.start();
         canalClientList.add(client);
     }
